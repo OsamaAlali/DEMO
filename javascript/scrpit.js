83d5556cc560b9d7941d5bc92256987cd3345995
osama-alali <osama.alali89@yahoo.com>
2021-04-04 17:41:44 +0300

    dmo

---
+++ javascript/scrpit.js
@@ -0,0 +1,9 @@
+let userName =prompt('what\'s your name');
+console.log(userName);
+let age=prompt('what\'s your age')
+console.log(age);
+let jobName=prompt('what\'s your job')
+console.log(age);
+let reason =prompt('why you are here?')
+console.log(age);
+alert("Hallo Mr."+userName+"your age is"+age+"you have great"+jobName+"job thank you to visted us ");
